44948908ab4138ac2d53c87c89b6075c391e6acc
jweiler281 <63027885+jweiler281@users.noreply.github.com>
2022-09-10 15:19:15 -0600

    worked out some of the list range issues but added a new one

--- minesweeper.py
+++ minesweeper.py
@@ -5,11 +5,12 @@ dim = 10
 mines = 10
 
 # TO DO Items
-#   - Determine field dimensions
-#   - Determine number of mines
-#   - Pick unique mine locations within field
+#   - Determine field dimensions -- done
+#   - Determine number of mines -- done
+#   - Pick unique mine locations within field -- done
 #   - Prompt user for coordinates of picks
-#   - Create list "array" of field
+#   - Create list "array" of field -- done
+#   - Add notation for area within 1 square of mines
 #   - Determine status of mines
 #   - Determine what is within an 3x3 grid of whichever place is selected
 #       - Reveal all mines if cell is mined
@@ -27,10 +28,10 @@ def setup():
     # Input validation check that dimension is within confines
     mine_ready = False
     # Input validation check that number of mines is within confines
-    dim_in = int(input("Enter dimensions for playing field (max 10, default is 10): "))
+    dim_in = int(input("Enter dimensions for playing field (min2, max 10, default is 10): "))
     # Receive input on dimensions of playing field
     # TO DO: catch and handle error if no number is input or non-integer is input
-    if dim_in < 0 or dim_in > 10:
+    if dim_in < 2 or dim_in > 10:
     # Validate dimension given by input
         dim_ready = False
     else:
@@ -94,37 +95,151 @@ def guess():
 def start_field():
 # Start troubleshooting here, will create dim x dim list and will put some of the mines into it
     global field
-    mine_symbol = '*'
-    field = [[0] * (len(range(dim))) for i in range(dim)]
+    # Tracks status of field and mines
+    mine_symbol = 0
+    # Annotates mine on field
+    field = [[-1] * (len(range(dim))) for i in range(dim)]
+    # Creates field with [[dim]dim] nested list
     counter = 0
-    while counter != (dim - 2): #len(mine_loc):
+    while counter != (mines):
         field[int(mine_loc[counter][1]) - 1][int(mine_loc[counter][0]) - 1] = mine_symbol
+        # Sets mine locations to 0 -- field[y][x]
         counter += 1
+    return field_pretty()
+
+def field_pretty():
+    global field
     field_output = ""
     for i in range(dim):
         field_output = field_output + str(i + 1) + ": " + str(field[i]) + "\n"
     return field_output
-    
-    #field[mine_loc[0][1]][mine_loc[0][0]] = '*' # first mine - field[y][x]
-    #field[mine_loc[1][1]][mine_loc[1][0]] = '*' # second mine - field[y][x]
-    #field[mine_loc[2][1]][mine_loc[2][0]] = '*' # third mine - field[y][x]
 
-#    counter = 0
-#    while counter != len(mine_loc):
-#        print("1: " + str(mine_loc) + "\n")
-#        print(mine_loc[0][1])
-#        print(mine_loc[1][1])
-#        print(mine_loc[2][1])
-#        counter += 1
+def aligner():
+    # Annotates squares 1 space from mines
+    global field
+    i = j = 0
+    while i != dim - 1:
+        y_counter = i
+        j = 0
+        while j != dim:
+            neighbors = [False, False, False, False, False, False, False, False]
+            square_sum = 0
+            if field[y_counter][j] == 0:
+                print("#0 i: " + str(i) + "\n" + "j: " + str(j))
+            elif i == 0 and j == 0: # Top left corner
+                print("#1 i: " + str(i) + "\n" + "j: " + str(j))
+                if field[y_counter][j + 1] == 0:
+                    neighbors[3] = True
+                if field[y_counter + 1][j + 1] == 0:
+                    neighbors[4] = True
+                if field[y_counter + 1][j] == 0:
+                    neighbors[5] = True
+            elif i == 0 and j == (dim - 1): # Top right corner
+                print("#2 i: " + str(i) + "\n" + "j: " + str(j))
+                if field[y_counter][j - 1] == 0:
+                    neighbors[7] = True
+                if field[y_counter + 1][j - 1] == 0:
+                    neighbors[6] = True
+                if field[y_counter + 1][j] == 0:
+                    neighbors[5] = True
+            elif i == 0: # Top row minus corners
+                print("#3 i: " + str(i) + "\n" + "j: " + str(j))
+                if field[y_counter][j - 1] == 0:
+                    neighbors[7] = True
+                if field[y_counter + 1][j - 1] == 0:
+                    neighbors[6] = True
+                if field[y_counter + 1][j] == 0:
+                    neighbors[5] = True
+                if field[y_counter + 1][ j + 1] == 0:
+                    neighbors[4] = True
+                if field[y_counter][j + 1] == 0:
+                    neighbors[3] = True
+            elif j == 0: # Left column minus corners
+                print("#4 i: " + str(i) + "\n" + "j: " + str(j))
+                if field[y_counter - 1][j] == 0:
+                    neighbors[1] = True
+                if field[y_counter - 1][j + 1] == 0:
+                    neighbors[2] = True
+                if field[y_counter][j + 1] == 0:
+                    neighbors[3] = True
+                if field[y_counter + 1][j + 1] == 0:
+                    neighbors[4] = True
+                if field[y_counter + 1][j] == 0:
+                        neighbors[5] = True
+            elif j == (dim - 1): # Right column minus corners
+                print("#5 i: " + str(i) + "\n" + "j: " + str(j))
+                if field[y_counter - 1][j] == 0:
+                    neighbors[1] = True
+                if field[y_counter - 1][j - 1] == 0:
+                    neighbors[0] = True
+                if field[y_counter][j - 1] == 0:
+                    neighbors[7] = True
+                if field[y_counter + 1][j - 1] == 0:
+                    neighbors[6] = True
+                if field[y_counter + 1][j] == 0:
+                    neighbors[5] = True
+            elif i == (dim - 1) and j == 0: # Bottom left corner
+                print("#6 i: " + str(i) + "\n" + "j: " + str(j))
+                if field[y_counter - 1][j] == 0:
+                    neighbors[1] = True
+                if field[y_counter - 1][j + 1] == 0:
+                    neighbors[2] = True
+                if field[y_counter][j + 1] == 0:
+                    neighbors[3] = True
+            elif i == (dim - 1) and j == (dim - 1): # Bottom right corner
+                print("#7 i: " + str(i) + "\n" + "j: " + str(j))
+                if field[y_counter][j - 1] == 0:
+                    neighbors[7] = True
+                if field[y_counter - 1][j - 1] == 0:
+                    neighbors[0] = True
+                if field[y_counter - 1][j] == 0:
+                    neighbors[1] = True
+            elif i == (dim - 1): # Bottom row minus corners
+                print("#8 i: " + str(i) + "\n" + "j: " + str(j))
+                if field[y_counter][j - 1] == 0:
+                    neighbors[7] = True
+                if field[y_counter - 1][j - 1] == 0:
+                    neighbors[0] = True
+                if field[y_counter - 1][j] == 0:
+                    neighbors[1] = True
+                if field[y_counter - 1][j + 1] == 0:
+                    neighbors[2] = True
+                if field[y_counter][j + 1] == 0:
+                    neighbors[3] = True
+            else:
+                print("#9 i: " + str(i) + "\n" + "j: " + str(j))
+                if field[y_counter][j - 1] == 0:
+                    neighbors[7] = True
+                if field[y_counter - 1][j - 1] == 0:
+                    neighbors[0] = True
+                if field[y_counter - 1][j] == 0:
+                    neighbors[1] = True
+                if field[y_counter - 1][j + 1] == 0:
+                    neighbors[2] = True
+                if field[y_counter][j + 1] == 0:
+                    neighbors[3] = True
+                if field[y_counter + 1][j + 1] == 0:
+                    neighbors[4] = True
+                if field[y_counter + 1][j] == 0:
+                    neighbors[5] = True
+                if field[y_counter + 1][j - 1] == 0:
+                    neighbors[6] = True
+            if True in neighbors:
+                for item in neighbors:
+                    if item == True:
+                        square_sum += 1
+                field[y_counter][j] = square_sum
+            j += 1
+        i += 1
 
 
 
 
-#setup()
-#display()
-#print(mine_setup())
-#print(guess())
+   
+
 setup()
 dog = mine_setup()
 print(dog)
 print(start_field())
+aligner()
+print(field_pretty())
